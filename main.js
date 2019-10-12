a481c375a63d8879b6307dfd8cc95c9425a31761
Kelebra28 <ricardokelebra@gmail.com>
2019-10-11 21:14:44 -0500

    Prueba de repo

---
+++ main.js
@@ -0,0 +1,10 @@
+// pwd -> es para mostrar la ruta en la que estas parado.
+// ls -> es para mostrar una lista del contenido de mi carpeta.
+// cd -> es par cambiar de carpeta.
+// clear -> limpia la terminal sin omitir comandos ya hechos.
+// cat -> muestra el contenido del un archivo.
+// mkdir -> crea una carpeta.
+// touch -> crear un archivo.
+// nano -> abre un editor de texto.
+// rm -rf -> borra las carpeta de forma recursiva.
+
